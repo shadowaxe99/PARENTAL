71fa542d50c4fb0bb2fb0604a2decdb0ed7ad367
Michael Gruen <michael.gruen9@gmail.com>
2023-12-04 08:15:27 -0500

    Revert "Duckie: make this more functional and fill in the logic"

--- src/components/NumberInput.tsx
+++ src/components/NumberInput.tsx
@@ -2,26 +2,22 @@ import React, { useState } from 'react';
 import './NumberInput.css';
 
 interface NumberInputProps {
-  label?: string;
-  value: number;
-  onChange: (newNumber: number) => void;
   maxNumber: number;
+  onNumberChange: (newNumber: number) => void;
 }
 
-const NumberInput: React.FC<NumberInputProps> = ({ label, value, onChange, maxNumber }) => {
-  const [inputValue, setInputValue] = useState<number>(value);
+const NumberInput: React.FC<NumberInputProps> = ({ maxNumber, onNumberChange }) => {
+  const [inputValue, setInputValue] = useState<number>(0);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = Math.min(maxNumber, Math.max(0, parseInt(event.target.value, 10) || 0));
     setInputValue(newValue);
-    onChange(newValue);
+    onNumberChange(newValue);
   };
 
   return (
     <div className="number-input-container">
-      {label && <label htmlFor="number-input">{label}</label>}
       <input
-        id="number-input"
         type="number"
         className="number-input"
         value={inputValue}
